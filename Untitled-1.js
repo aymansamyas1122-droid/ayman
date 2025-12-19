88b5c30959a29b610b1466cb19cea50028a70819
aymansamyas1122-droid <aymansamyas1122@gmail.com>
2025-12-19 23:50:50 +0200

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,127 @@
+document.writeln("<h1>CODE EXECUTED</h1>"); 
+
+let developerName = "Ayman Samy";
+let currentYear = 2025;
+let debugCheck = true;
+
+
+let n = null;
+console.log(typeof n)
+console.log( n ==null)
+
+
+ let f =  10;
+ let t = 11;
+console.log( f ==t)
+
+
+
+
+let i = 13;
+console.log(typeof i)
+
+i = 23n
+console.log(typeof i)
+
+let u = 29;
+ u = String(u);
+ console.log(typeof u)
+
+function hello(){
+    console.log("hello")
+}
+hello()
+
+function add(n1=2,n2=3){
+    var c=3
+    var val =n1 + n2
+    console.log(val)
+}
+add()
+console.log(add())
+{
+
+
+}
+
+function multiply(num1,num2){
+    let s=7
+    var val =num1 *num2
+    return val
+}
+console.log(s)
+multiply (3,4)
+
+
+ console .log (multiply (3,4))
+
+ var y= function (){
+    console.log("this is y")
+ }
+
+ y()
+
+ function  callAnotherFn(Fn){
+    console.log("call another function")
+    Fn()
+
+
+ }
+ callAnotherFn( ()=> {
+    console.log("this is a parameter")
+ 
+
+ })
+
+ for(let i=0; i<5;i++){
+    var d=2
+    console.log(i);
+
+ }
+ console.log(d)
+
+
+
+var d = 2
+let r = 2
+
+
+
+var arr=[1,2,3,"name","string"]
+
+
+console.log(arr[2])
+
+
+console.log(arr,length)
+
+
+
+arr.pop()
+
+
+
+
+console.log(arr)
+
+
+
+
+
+arr.push(90,70)
+
+
+
+console.log(arr)
+arr.splice(3,2)
+
+console.log(arr)
+
+
+arr.shift()
+console.log(arr)
+arr.unshift("firist")
+
+
+console.log(arr)
+
